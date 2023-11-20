d26acd08e08a601461bcd2daba3c5d4e0aa4b067
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-11-20 13:23:39 +0100

    add text in the book
    Bug with the character and the book

--- src/Morse's room/Rooms/FirstRoom/Scripts/Global.cs
+++ src/Morse's room/Rooms/FirstRoom/Scripts/Global.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class sceneController : Node
+public partial class Global : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
