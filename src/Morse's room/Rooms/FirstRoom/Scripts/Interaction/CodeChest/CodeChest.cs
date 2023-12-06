fdc834db46d33d9939f196e107465211dd45f453
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-12-06 09:36:04 +0100

    refactor CodeChest

--- src/Morse's room/Rooms/FirstRoom/Scripts/Interaction/CodeChest/CodeChest.cs
+++ src/Morse's room/Rooms/FirstRoom/Scripts/Interaction/CodeChest/CodeChest.cs
@@ -1,44 +1,42 @@
 using Godot;
 using System;
 
+using TemporalIT.Scripts.EButton;
+
 namespace TemporalIT.Morse_s_room.Rooms.FirstRoom.Scripts.Interaction.CodeChest
 {
     public partial class Book : Node
     {
+        private EButton _keyboardInteration;
+		
+        private const String _NEXT_SCENE_PATH = "res://Morse's room/Rooms/FirstRoom/CodeChest.tscn";
+        private const String _E_BUTTON = "button_e";
+        private const String _NAME_OF_THE_ANIMATION = "default";
+        
         // Called when the node enters the scene tree for the first time.
-        private AnimatedSprite2D _keyboardInteration;
 
         public override void _Ready()
         {
-            _keyboardInteration = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+            _keyboardInteration = new EButton(GetNode<AnimatedSprite2D>("AnimatedSprite2D"));
         }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
         public override void _Process(double delta)
         {
-            if (Input.IsActionPressed("button_e") && _keyboardInteration.IsVisibleInTree())
+            if (Input.IsActionPressed(_E_BUTTON) && _keyboardInteration.isVisible())
             {
-                GetTree().ChangeSceneToFile("res://Morse's room/Rooms/FirstRoom/CodeChest.tscn");
+                GetTree().ChangeSceneToFile(_NEXT_SCENE_PATH);
             }
         }
 
         public void _on_body_entered(CharacterBody2D body)
         {
-            // Replace with function body.
-            available();
-        }
-
-        private void available()
-        {
-            _keyboardInteration.Visible = true;
-            _keyboardInteration.Play("default");
+            _keyboardInteration.available(_NAME_OF_THE_ANIMATION);
         }
 
         public void _on_body_exited(CharacterBody2D body)
         {
-            // Replace with function body.
-            _keyboardInteration.Visible = false;
-            _keyboardInteration.Stop();
+            _keyboardInteration.disable();
         }
     }
 }
