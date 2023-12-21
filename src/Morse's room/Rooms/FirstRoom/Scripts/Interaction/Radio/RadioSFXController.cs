f2dab5dc4a531b4842faea215cc8aa96536ae6ff
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-12-21 15:27:16 +0100

    charge elements of the RadioMorse

--- src/Morse's room/Rooms/FirstRoom/Scripts/Interaction/Radio/RadioSFXController.cs
+++ src/Morse's room/Rooms/FirstRoom/Scripts/Interaction/Radio/RadioSFXController.cs
@@ -3,12 +3,24 @@ using System;
 
 public partial class RadioSFXController : VBoxContainer
 {
-	private const String _PLAY_BUTTON_PATH = "";
-	private const String _PAUSE_BUTTON_PATH = "";
-	private const String _RETRY_BUTTON_PATH = "";
+	//Elements
+	private Button _playButton;
+	private Button _pauseButton;
+	private Button _retryButton;
+	private AudioStreamPlayer _morseCodeSound;
+	
+	//Const
+	private const String _PLAY_BUTTON_PATH = "ButtonPlayPauseRetry/Play";
+	private const String _PAUSE_BUTTON_PATH = "ButtonPlayPauseRetry/Pause";
+	private const String _RETRY_BUTTON_PATH = "ButtonPlayPauseRetry/Retry";
+	private const String _MORSE_CODE_SOUND_PATH = "AudioStreamPlayer";
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_playButton = GetNode<Button>(_PLAY_BUTTON_PATH);
+		_pauseButton = GetNode<Button>(_PAUSE_BUTTON_PATH);
+		_retryButton = GetNode<Button>(_RETRY_BUTTON_PATH);
+		_morseCodeSound = GetNode<AudioStreamPlayer>(_MORSE_CODE_SOUND_PATH);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
