29e65ee78353a3ece03d4e116e96d99cddb9e719
Lucas <lucas.theodore2004@gmail.com>
2023-12-21 15:27:06 +0100

    Keep building DialogBox script

--- src/ButtonClose.cs
+++ src/ButtonClose.cs
@@ -3,6 +3,12 @@ using System;
 
 public partial class ButtonClose : Button
 {
+	
+	
+	public override void _Ready()
+	{
+		DialogBox.disable();
+	}
 	public void _on_pressed()
 	{
 		NewsPaper.setInvisibleNewsPaper();
--- src/Scripts/DialogBox/DialogBox.cs
+++ src/Scripts/DialogBox/DialogBox.cs
@@ -4,18 +4,27 @@ using System.Data;
 
 public partial class DialogBox : Node
 {
-	private Sprite2D _dialogBox;
+	static private Sprite2D _dialogBox;
 	private Label _labelText;
 	private Label _labelName;
-	private AnimationPlayer _animationPlayer;
+	static private AnimationPlayer _animationPlayer;
+
+	public DialogBox(Sprite2D dialogBox, Label labelText, Label labelName)
+	{
+		_dialogBox = dialogBox;
+		_labelText = labelText;
+		_labelName = labelName;
+	}
 	
-	
+	public static void disable()
+	{
+		_dialogBox.Visible = false;
+		_animationPlayer.Stop();
+	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-		_dialogBox = GetNode<Sprite2D>("DialogBox");
-		_labelText = GetNode<Label>("LabelText");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,19 +34,15 @@ public partial class DialogBox : Node
 		_animationPlayer.Play(nameOfTheAnimation);
 	}
 
-	public void disable()
-	{
-		_dialogBox.Visible = false;
-		_animationPlayer.Stop();
-	}
+	
 
 	public bool isVisible()
 	{
 		return _dialogBox.IsVisibleInTree();
 	}
 
-	public void setText(String text)
+	public void setText(String name, String text)
 	{
-		
+		_labelName.Text = name;
 	}
 }
