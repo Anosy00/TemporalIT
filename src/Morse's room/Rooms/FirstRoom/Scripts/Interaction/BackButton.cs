6bb1f1a4c1486b292a352cd42039ae81ba9636c5
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-12-21 15:12:47 +0100

    Refactor BackButton, finished MorseInstruction scene

--- src/Morse's room/Rooms/FirstRoom/Scripts/Interaction/BackButton.cs
+++ src/Morse's room/Rooms/FirstRoom/Scripts/Interaction/BackButton.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class BackButtonCodeChest : Button
+public partial class BackButton : Button
 {
 	private const String _NEXT_SCENE_PATH = "res://Morse's room/Rooms/FirstRoom/FirstRoom.tscn";
 	public void _on_back_button_pressed()
