4d0aff3981b458e9c6853b4db71c431fedb2ccae
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-12-21 15:16:32 +0100

    add RadioSFXController

--- src/Morse's room/Rooms/FirstRoom/Scripts/Interaction/Radio/RadioSFXController.cs
+++ src/Morse's room/Rooms/FirstRoom/Scripts/Interaction/Radio/RadioSFXController.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class RadioSFXController : VBoxContainer
 {
+	private const String _PLAY_BUTTON_PATH = "";
+	private const String _PAUSE_BUTTON_PATH = "";
+	private const String _RETRY_BUTTON_PATH = "";
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
