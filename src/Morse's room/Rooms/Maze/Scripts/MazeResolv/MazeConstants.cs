68f7893f95d15b51126a631ca8ef1653c413af35
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2024-01-09 12:29:35 +0100

    create class MazeConstant and MazeResolv

---
+++ src/Morse's room/Rooms/Maze/Scripts/MazeResolv/MazeConstants.cs
@@ -0,0 +1,37 @@
+﻿
+namespace TemporalIT.Morse_s_room.Rooms.Maze.Scripts.MazeResolv
+{
+    public class MazeConstants
+    {
+        private const int _LIMIT_COLUMN_MAX = 7;
+        private const int _FREE_CASE = 0;
+        private const int _PLAYER_POSITION = 1;
+        private const int _MONSTER_POSITION = 2;
+
+        public static int[][] fill()
+        {
+            int[][] _maze = new int[7][];
+            int[] borderTopBot = new int[_LIMIT_COLUMN_MAX] {_MONSTER_POSITION,_MONSTER_POSITION,_MONSTER_POSITION,_MONSTER_POSITION,_MONSTER_POSITION,_MONSTER_POSITION,_MONSTER_POSITION};
+            _maze[0] = borderTopBot;
+            _maze[1] = new int[_LIMIT_COLUMN_MAX]
+            {
+                _MONSTER_POSITION, _FREE_CASE, _FREE_CASE, _FREE_CASE, _MONSTER_POSITION, _MONSTER_POSITION,
+                _MONSTER_POSITION
+            };
+            _maze[2] = new int[_LIMIT_COLUMN_MAX]
+            {
+                _MONSTER_POSITION,_FREE_CASE,_MONSTER_POSITION,_FREE_CASE,_FREE_CASE,_MONSTER_POSITION,_MONSTER_POSITION
+            };
+            _maze[3] = new int[_LIMIT_COLUMN_MAX]
+            {
+                _MONSTER_POSITION,_FREE_CASE,_FREE_CASE,_MONSTER_POSITION,_FREE_CASE,_FREE_CASE,10
+            };
+            _maze[4] = new int[_LIMIT_COLUMN_MAX]
+            {
+                _FREE_CASE,_FREE_CASE,_FREE_CASE,_MONSTER_POSITION,_MONSTER_POSITION,_MONSTER_POSITION,_MONSTER_POSITION
+            };
+            _maze[5] = borderTopBot;
+            return _maze;
+        }
+    }
+}
--- src/Morse's room/Rooms/Maze/Scripts/MazeResolv/MazeResolv.cs
+++ src/Morse's room/Rooms/Maze/Scripts/MazeResolv/MazeResolv.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using TemporalIT.Morse_s_room.Rooms.Maze.Scripts.MazeResolv;
 
 namespace TemporalIT.Morse_s_room.Rooms.Maze.Scripts.MazeResolv
 {
@@ -7,13 +8,15 @@ namespace TemporalIT.Morse_s_room.Rooms.Maze.Scripts.MazeResolv
 	{
 		private int[][] _maze;
 
+		private const int _LIMIT_COLUMN_MAX = 7;
 		private const int _FREE_CASE = 0;
 		private const int _PLAYER_POSITION = 1;
 		private const int _MONSTER_POSITION = 2;
 
 		public MazeResolv()
 		{
-			
+			_maze = MazeConstants.fill();
 		}
+		
 	}
 }
