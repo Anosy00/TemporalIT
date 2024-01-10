6f0bf42644a5dfef62984fa941e57a049bb12125
Rémi Lhuissier <remi.lhuissier@etu.unilim.fr>
2024-01-10 15:32:01 +0100

    add graphical e button and remove the ability to use escape

--- src/Scripts/ScriptsMuseum/Card/ButtonPunshedCard.cs
+++ src/Scripts/ScriptsMuseum/Card/ButtonPunshedCard.cs
@@ -6,6 +6,8 @@ public partial class ButtonPunshedCard : Node
 	// Called when the node enters the scene tree for the first time.
 	private Button _buttonPunshedCard;
 	private String sceneSuivante = "res://TilesMapMusee/mapMusee.tscn";
+	public AnimatedSprite2D _keyboardInteration;
+	
 	public override void _Ready()
 	{
 		_buttonPunshedCard = GetNode<Button>("ButtonPunshedCard");
@@ -14,6 +16,9 @@ public partial class ButtonPunshedCard : Node
 			// Simulate releasing the E button to disable the action
 			Input.ActionRelease("button_e");
 		}
+		_keyboardInteration = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_keyboardInteration.Visible = true;
+		_keyboardInteration.Play("default");
 		
 	}
 
@@ -21,7 +26,7 @@ public partial class ButtonPunshedCard : Node
 	public override void _Process(double delta)
 	{
 		
-		if (Input.IsActionPressed("echap") || Input.IsActionPressed("button_e") ) 
+		if (Input.IsActionPressed("button_e") ) 
 		{
 			GetTree().ChangeSceneToFile(sceneSuivante);
 				
