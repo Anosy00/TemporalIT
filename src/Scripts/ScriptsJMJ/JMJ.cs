db3954d05ad8c964b2e986ca153b5534c275d401
Lucas <lucas.theodore2004@gmail.com>
2024-01-15 09:22:12 +0100

    Keep Building the interaction with JMJ

--- src/Scripts/ScriptsJMJ/JMJ.cs
+++ src/Scripts/ScriptsJMJ/JMJ.cs
@@ -60,7 +60,7 @@ public partial class JMJ : Area2D
 			case 1:
 				if (GlobalJMJ.nbPlanks < 2)
 				{
-					GlobalJMJ.displayDialogBox(Global.playerName, "Je m'appelle " + Global.playerName + " il me semble que j'ai été envoyé dans le temps !", _dialogBox, _timer);
+					GlobalJMJ.displayDialogBox(Global.playerName, "Je m'appelle " + Global.playerName + " il me semble que j'ai voyagé dans le temps !", _dialogBox, _timer);
 					_timer.Start();
 				}
 				else
@@ -70,11 +70,11 @@ public partial class JMJ : Area2D
 				}
 				break;
 			case 2:
-				GlobalJMJ.displayDialogBox("???", "Le voyage dans le temps ?! C'est impossible, mais bon passons j'ai un problème plus important. ", _dialogBox, _timer);
+				GlobalJMJ.displayDialogBox("???", "Le voyage dans le temps ?! C'est impossible, mais bon passons j'ai un problème plus important... ", _dialogBox, _timer);
 				_timer.Start();
 				break;
 			case 3:
-				GlobalJMJ.displayDialogBox("Jacquard", "Je m'appelle Jean Marie Jacquard et j'ai un problème dans la construction de ma machine, aide moi à la construire !", _dialogBox, _timer);
+				GlobalJMJ.displayDialogBox("Jacquard", "Je m'appelle Jean Marie Jacquard et j'ai un problème dans la construction de ma machine, aide moi à la construire !...", _dialogBox, _timer);
 				_timer.Start();
 				break;
 			case 4:
@@ -83,6 +83,11 @@ public partial class JMJ : Area2D
 				GlobalJMJ.canInteractWithPlanks = true;
 				break;
 			case 5:
+				GlobalJMJ.displayDialogBox("Jacquard", "Merci, il nous manque uniquement du fil, tu peux en trouver dans mon coffre en haut...", _dialogBox, _timer);
+				_timer.Start();
+				break;
+			case 6:
+				GlobalJMJ.displayDialogBox("Jacquard", "Merci, il nous manque uniquement du fil, tu peux en trouver dans mon coffre en haut...", _dialogBox, _timer);
 				break;
 		}
 		
