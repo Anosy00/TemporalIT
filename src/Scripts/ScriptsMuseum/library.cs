e683bd6652a45578f83bfd87688b6684a504f758
Rémi Lhuissier <remi.lhuissier@etu.unilim.fr>
2024-01-15 16:45:38 +0100

    recovery coordinates collision shape

---
+++ src/Scripts/ScriptsMuseum/library.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class library : Node
+{
+	private CollisionShape2D collisionShape;
+	private Vector2 collisionShapePosition;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+		if (collisionShape != null)
+		{
+			// Obtenez les coordonnées globales de l'élément
+			collisionShapePosition = collisionShape.GlobalPosition;
+
+			// Affichez les coordonnées
+			GD.Print("Coordonnées de l'élément : (", getX(),", ", getY(),")");
+		}
+		else
+		{
+			GD.Print("L'élément n'a pas été correctement référencé.");
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public float getY()
+	{
+		return collisionShapePosition[1]-323;
+	}
+	public float getX()
+	{
+		return collisionShapePosition[0]-323;
+	}
+}
