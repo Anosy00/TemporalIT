fda5712da76204af7d04c2d0a137c31cf573afdc
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2024-01-16 14:28:38 +0100

    fixed the path of the text in FinalText.cs

--- src/Morse's room/Rooms/LastScene/Scripts/FinalText.cs
+++ src/Morse's room/Rooms/LastScene/Scripts/FinalText.cs
@@ -8,7 +8,7 @@ public partial class FinalText : CenterContainer
 	private Label _text;
 
 	private const String _PATH_ANIMATION = "AnimationText";
-	private const String _PATH_TEXT = "Label";
+	private const String _PATH_TEXT = "Camera2D/Label";
 	private const String _PATH_CREDITS_SCENE = "res://Credits/CreditsScene.tscn";
 	
 	private const String _NAME_OF_THE_ANIMATION = "text-animation";
