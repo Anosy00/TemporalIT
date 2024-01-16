b96fd795881ca57570c233352ded9f3fe23603d2
Alexandre Billonnet <alexandre.billonnet@etu.unilim.fr>
2024-01-16 15:41:39 +0100

    fixing bug in credits scene

--- src/Scripts/Credits/CreditsScene.cs
+++ src/Scripts/Credits/CreditsScene.cs
@@ -1,14 +1,16 @@
-using System;
 using Godot;
-namespace TemporalIT.Scripts.CreditsScene;
+namespace TemporalIT.Scripts.Credits;
 
-public partial class CreditsScene : VBoxContainer
+public partial class CreditsScene : Control
 {
+    private VBoxContainer _vBoxContainer;
     private const float Speed = 6f;
     private Vector2I _screenSize;
     private AnimatedSprite2D _animateSprite;
     private RichTextLabel _label;
     private TextureRect _textureRect;
+    private CenterContainer _centerContainer;
+    private PanelContainer _panelContainer;
     private int _index;
     private CharacterBody2D _characterBody2D;
     private AnimationPlayer _animationLabel;
@@ -16,7 +18,7 @@ public partial class CreditsScene : VBoxContainer
     {
         new[]
         {
-            "[font_size=70][center]Programming Team\n",
+            "[font_size=70][center]Equipe de programmation\n",
             "[font_size=40]Alexandre Billonnet\n",
             "Lucas Theodore\n",
             "Matias Deveze\n",
@@ -25,7 +27,7 @@ public partial class CreditsScene : VBoxContainer
         },
         new[]
         {
-            "[center][font_size=70]Voice Actors\n",
+            "[center][font_size=70]Doubleurs\n",
             "[font_size=40]Logan Ferreira\n",
             "Sofia Aylal\n",
             "Matys Deschamps"
@@ -33,18 +35,21 @@ public partial class CreditsScene : VBoxContainer
         new []
         {
             "[center][font_size=70]TemporalIT\n",
-            "[font_size=40]Thanks for playing!"
+            "[font_size=40]Merci d'avoir joué !"
         }
     };
     
     public override void _Ready()
     {
         base._Ready();
+        _centerContainer = GetNode<CenterContainer>("CenterContainer");
+        _panelContainer = _centerContainer.GetNode<PanelContainer>("PanelContainer");
+        _vBoxContainer = _panelContainer.GetNode<VBoxContainer>("VBoxContainer");
         _characterBody2D = GetNode<CharacterBody2D>("Player");
         _screenSize = DisplayServer.WindowGetSize();
         _animateSprite = _characterBody2D.GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        _label = GetNode<RichTextLabel>("RichTextLabel");
-        _textureRect = GetNode<TextureRect>("TextureRect");
+        _label = _vBoxContainer.GetNode<RichTextLabel>("RichTextLabel");
+        _textureRect = _vBoxContainer.GetNode<TextureRect>("TextureRect");
         _animationLabel = _label.GetNode<AnimationPlayer>("AnimationPlayer");
         ResetPlayerResetPosition();
     }
@@ -57,7 +62,7 @@ public partial class CreditsScene : VBoxContainer
         };
         _animateSprite.Play("moveRight");
         _characterBody2D.MoveAndCollide(velocity);
-        if (_characterBody2D.Position.X > _screenSize.X * 0.8)
+        if (_characterBody2D.Position.X > _screenSize.X * 1.05)
         {
             ResetPlayerResetPosition();
         }
@@ -72,7 +77,7 @@ public partial class CreditsScene : VBoxContainer
         }
         ReloadLabel(_index);
         _index += 1;
-        _characterBody2D.Position = new Vector2((float)Math.Round(_screenSize.X * -0.3), (int)Math.Round(_screenSize.Y * 0.7));
+        _characterBody2D.Position = new Vector2((float)Math.Round(_screenSize.X * -0.05), (int)Math.Round(_screenSize.Y * 0.95));
     }
     
     private void ReloadLabel(int index)
