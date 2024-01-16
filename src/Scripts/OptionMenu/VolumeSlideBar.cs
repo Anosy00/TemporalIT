a7621c9c26dffe1dc49f2d15c904317f7d374e3a
ZenkoLespadon <lefebvre.william82@gmail.com>
2024-01-16 14:04:01 +0100

    fix bugfix of "assembly bug"

--- src/Scripts/OptionMenu/VolumeSlideBar.cs
+++ src/Scripts/OptionMenu/VolumeSlideBar.cs
@@ -3,23 +3,23 @@ namespace TemporalIT.Scripts.OptionMenu;
 
 public partial class VolumeSlideBar : HSlider
 {
-    private int _masterBus = AudioServer.GetBusIndex("Master");
-    
-    public override void _Ready()
-    {
-        base._Ready();
-        Disconnect("value_changed", new Callable(this, "_on_value_changed"));
-        Connect("value_changed", new Callable(this, "_on_value_changed"));
-        Value = 0.5f;
-    }
-    
-    private static float _linear_to_db(float value)
-    {
-        return 20 * Mathf.Log(value);
-    }
-    
-    private void _on_value_changed(float value)
-    {
-        AudioServer.SetBusVolumeDb(_masterBus, _linear_to_db(value));
-    }
+	private int _masterBus = AudioServer.GetBusIndex("Master");
+	
+	public override void _Ready()
+	{
+		base._Ready();
+		Disconnect("value_changed", new Callable(this, "_on_value_changed"));
+		Connect("value_changed", new Callable(this, "_on_value_changed"));
+		Value = 0.5f;
+	}
+	
+	private static float _linear_to_db(float value)
+	{
+		return 20 * Mathf.Log(value);
+	}
+	
+	private void _on_value_changed(float value)
+	{
+		AudioServer.SetBusVolumeDb(_masterBus, _linear_to_db(value));
+	}
 }
--- src/Scripts/OptionMenu/WindowMenu.cs
+++ src/Scripts/OptionMenu/WindowMenu.cs
@@ -3,83 +3,83 @@ namespace TemporalIT.Scripts.OptionMenu;
 
 public partial class WindowMenu : OptionButton
 {
-    private void Fullscreen()
-    {
-        DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
-        OptionButtonDisabled(true);
-    }
-    
-    private void Maximized()
-    {
-        DisplayServer.WindowSetMode(DisplayServer.WindowMode.Maximized);
-        OptionButtonDisabled(true);
-    }
-    
-    private void Windowed()
-    {
-        DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
-        OptionButtonDisabled(false);
-    }
-    
-    private void ConfigureFullscreenMode()
-    {
-        Select(0);
-        OptionButtonDisabled(true);
-    }
+	private void Fullscreen()
+	{
+		DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+		OptionButtonDisabled(true);
+	}
+	
+	private void Maximized()
+	{
+		DisplayServer.WindowSetMode(DisplayServer.WindowMode.Maximized);
+		OptionButtonDisabled(true);
+	}
+	
+	private void Windowed()
+	{
+		DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+		OptionButtonDisabled(false);
+	}
+	
+	private void ConfigureFullscreenMode()
+	{
+		Select(0);
+		OptionButtonDisabled(true);
+	}
 
-    private void ConfigureMaximizedMode()
-    {
-        Select(1);
-        OptionButtonDisabled(true);
-    }
+	private void ConfigureMaximizedMode()
+	{
+		Select(1);
+		OptionButtonDisabled(true);
+	}
 
-    private void ConfigureWindowedMode()
-    {
-        Select(2);
-        OptionButtonDisabled(false);
-    }
+	private void ConfigureWindowedMode()
+	{
+		Select(2);
+		OptionButtonDisabled(false);
+	}
 
-    public override void _Ready()
-    {
-        DisplayServer.WindowMode currentWindowMode = DisplayServer.WindowGetMode();
-        switch (currentWindowMode)
-        {
-            case DisplayServer.WindowMode.Fullscreen:
-                ConfigureFullscreenMode();
-                break;
-            case DisplayServer.WindowMode.Maximized:
-                ConfigureMaximizedMode();
-                break;
-            case DisplayServer.WindowMode.Windowed:
-                ConfigureWindowedMode();
-                break;
-            default:
-                GD.PrintErr("Invalid window mode");
-                break;
-        }
-    }
+	public override void _Ready()
+	{
+		DisplayServer.WindowMode currentWindowMode = DisplayServer.WindowGetMode();
+		switch (currentWindowMode)
+		{
+			case DisplayServer.WindowMode.Fullscreen:
+				ConfigureFullscreenMode();
+				break;
+			case DisplayServer.WindowMode.Maximized:
+				ConfigureMaximizedMode();
+				break;
+			case DisplayServer.WindowMode.Windowed:
+				ConfigureWindowedMode();
+				break;
+			default:
+				GD.PrintErr("Invalid window mode");
+				break;
+		}
+	}
 
-    private void OptionButtonDisabled(bool value)
-    {
-        GetParent().GetNode<OptionButton>("ResolutionMenu").Disabled = value;
-    }
+	private void OptionButtonDisabled(bool value)
+	{
+		GetParent().GetNode<OptionButton>("ResolutionMenu").Disabled = value;
+	}
 
-    private void _on_item_selected(int index)
-    {
-        switch (index)
-        {
-            case 0:
-                Fullscreen();
-                break;
-            case 1:
-                Maximized();
-                break;
-            case 2:
-                Windowed();
-                break;
-            default:
-                GD.PrintErr("Invalid fullscreen index");
-                break;
-        }
-    }
+	private void _on_item_selected(int index)
+	{
+		switch (index)
+		{
+			case 0:
+				Fullscreen();
+				break;
+			case 1:
+				Maximized();
+				break;
+			case 2:
+				Windowed();
+				break;
+			default:
+				GD.PrintErr("Invalid fullscreen index");
+				break;
+		}
+	}
 }
