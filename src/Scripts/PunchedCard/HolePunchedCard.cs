691abb387671e39098c28d3ed463062e249fa4df
Rémi Lhuissier <remi.lhuissier@etu.unilim.fr>
2024-02-19 17:55:10 +0100

    WIP dynamic holes display

--- src/Scripts/PunchedCard/HolePunchedCard.cs
+++ src/Scripts/PunchedCard/HolePunchedCard.cs
@@ -5,8 +5,9 @@ public partial class HolePunchedCard : Node
 {
 	private int numberOfHoles;
 	private String nameOfPicture;
-	private AnimatedSprite2D nodeOfHole;
-	
+	private AnimatedSprite2D nodeOfHolePlayed;
+	private int frame;
+	int nbFrame=17;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -26,22 +27,32 @@ public partial class HolePunchedCard : Node
 			while (j != numberOfHoles)
 			{
 				j = j + 1;
+				frame=0;
 				//GD.Print(i);
 				nameOfPicture = "TheHoles/AnimatedSprite2D" + j; //HolePunchedCard
-				nodeOfHole = (AnimatedSprite2D)GetNode(nameOfPicture);
-				
-				if (j <= Global.getRoomNumber() )
+				AnimatedSprite2D nodeOfHole = (AnimatedSprite2D)GetNode(nameOfPicture);
+				//GD.Print("j = "+j+", nodeOfHole= "+nodeOfHole.Name+" nbframe= "+nbFrame+" Global.getRoomNumber()= "+Global.getRoomNumber()+" numberOfHoles= "+numberOfHoles);
+				if (j < Global.getRoomNumber() )
 				{
+					//GD.Print("inferieur");
 					nodeOfHole.Visible = true;
-					if (j == Global.getRoomNumber()){
-						nodeOfHole.Stop();
-						nodeOfHole.Play("default");
-					}
+					nodeOfHole.Frame = nbFrame - 1;
 					
-				}else
+				}else  if (j == Global.getRoomNumber()){
+					//GD.Print("egale");
+					nodeOfHole.Visible = true;
+					nodeOfHole.Stop();
+					nodeOfHole.Play("default");
+					nodeOfHolePlayed = nodeOfHole;
+					//nbFrame=nodeOfHolePlayed.Frames;
+						
+				} else 
 				{
+					//GD.Print("superieur");
 					nodeOfHole.Visible = false;
 				}
+					
+				
 				//GD.Print(nameOfPicture,", ",j,", ",nodeOfHole.Visible);
 
 			}
@@ -57,10 +68,27 @@ public partial class HolePunchedCard : Node
 	{
 		
 	}
-	private void _on_animated_sprite_2d_animation_finished()
-	{
-		nodeOfHole.Stop();
-	}
 	
+	/*private void _on_animated_sprite_2d_animation_looped()
+	{
+		int nombreFrames = nodeOfHolePlayed.Frames.GetFrameCount();
+		
+		GD.Print("annimation looped");
+		GD.Print(nodeOfHolePlayed.Name);
+		nodeOfHolePlayed.Stop();
+		//nodeOfHolePlayed.Frame = nombreFrames - 1;
+	}*/
+	private void _on_animated_sprite_2d_frame_changed()
+	{
+		
+		//GD.Print(nodeOfHolePlayed.Name);
+		//GD.Print(frame+", "+nbFrame);
+		if (frame == nbFrame){
+			//GD.Print("egale");
+			nodeOfHolePlayed.Pause();
+			//nodeOfHolePlayed.Frame = nbFrame;
+			
+		}
+		frame +=1;
+	}
 }
-
--- src/Scripts/TitleScreen/PlayButton.cs
+++ src/Scripts/TitleScreen/PlayButton.cs
@@ -1,16 +1,16 @@
-﻿using Godot;
+using Godot;
 
 namespace TemporalIT.Scripts.TitleScreen;
 
 public partial class PlayButton : Button
 {
-    private void _on_start_button_pressed()
-    {
-        if (GetTree().ChangeSceneToFile("res://Museum/MuseumMap.tscn") != Error.Ok)
-            GD.PrintErr("Failed to change scene to MuseumMap.tscn");
-        else
-        {
-            GetTree().ChangeSceneToFile("res://Museum/MuseumMap.tscn");
-        }
-    }
+	private void _on_start_button_pressed()
+	{
+		if (GetTree().ChangeSceneToFile("res://Museum/MuseumMap.tscn") != Error.Ok)
+			GD.PrintErr("Failed to change scene to MuseumMap.tscn");
+		else
+		{
+			GetTree().ChangeSceneToFile("res://Museum/MuseumMap.tscn");
+		}
+	}
 }
