6c6d772c9010906573d506d7712920f8ccee832f
ZenkoLespadon <lefebvre.william82@gmail.com>
2024-03-18 14:21:46 +0100

    start of json language system (WIP)

--- src/Scripts/TitleScreen/CreditsButton.cs
+++ src/Scripts/TitleScreen/CreditsButton.cs
@@ -3,13 +3,13 @@ namespace TemporalIT.Scripts.TitleScreen;
 
 public partial class CreditsButton : Button
 {
-    private void _on_pressed()
-    {
-        if (GetTree().ChangeSceneToFile("res://Credits/CreditsScene.tscn") != Error.Ok)
-            GD.PrintErr("Failed to change scene to CreditsScene.tscn");
-        else
-        {
-            GetTree().ChangeSceneToFile("res://Credits/CreditsScene.tscn");
-        }
-    }
+	private void _on_pressed()
+	{
+		if (GetTree().ChangeSceneToFile("res://Credits/CreditsScene.tscn") != Error.Ok)
+			GD.PrintErr("Failed to change scene to CreditsScene.tscn");
+		else
+		{
+			GetTree().ChangeSceneToFile("res://Credits/CreditsScene.tscn");
+		}
+	}
 }
